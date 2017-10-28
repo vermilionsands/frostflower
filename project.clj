0e8d43394e2ef407d12df4b6c2af6ee34fb1cd88
vermilionsands <sands.vermilion@gmail.com>
2017-10-28 20:42:18 +0200

    Adding project url.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject vermilionsands/frostflower "0.1.0"
   :description "A local durable atom backend for duratom."
+  :url "https://github.com/vermilionsands/frostflower"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
