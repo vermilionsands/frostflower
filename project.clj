743e81e2805b6a576d918d5d2eff5e12dd647284
vermilionsands <sands.vermilion@gmail.com>
2018-03-08 19:48:00 +0100

    Bump version, move clojure dependency to dev profile

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject vermilionsands/frostflower "0.1.2"
+(defproject vermilionsands/frostflower "0.1.3"
   :description "A local durable atom backend for duratom."
   :url "https://github.com/vermilionsands/frostflower"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [duratom "0.3.5"]])
+  :dependencies [[duratom "0.3.5"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})
