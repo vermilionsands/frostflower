913ee4bd690227ed4c47a1f3c7382c295d219b38
vermilionsands <sands.vermilion@gmail.com>
2018-03-08 19:47:38 +0100

    Add a test that reproduces a situation I had on a Windows box (which failed...)

--- test/vermilionsands/frostflower_test.clj
+++ test/vermilionsands/frostflower_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.java.io :as io]
             [clojure.test :refer [is deftest]]
             [duratom.core :as duratom]
+            [duratom.utils :as utils]
             [vermilionsands.frostflower :as frostflower])
   (:import [java.io File]
            [java.util.concurrent CountDownLatch]))
@@ -9,10 +10,11 @@
 (defn- slurp-data [path]
   (-> path slurp read-string))
 
-(defn- default-durable [file & [init]]
+(defn- default-durable [file & [init rw]]
   (duratom/duratom
     :sync-local-file
     :init (or init {:a 0})
+    :rw (merge @#'duratom.core/default-file-rw rw)
     :file-path (.getAbsolutePath file)))
 
 (defn- future-swap [^CountDownLatch start ^CountDownLatch done a f & args]
@@ -71,3 +73,24 @@
              (set @durable)))
       (is (= (set (range n))
              (set (slurp-data tmp-file)))))))
+
+(defn- slow-write [filepath data]
+  (Thread/sleep (rand-int 100))
+  (utils/write-edn-to-file! filepath data))
+
+;; something similar to this test
+;; failed for me on a Windows box, but I'm not able to reproduce it
+;; on a linux, strange...
+(deftest test-with-slow-serializier
+  (with-temp-file tmp-file
+    (let [n 100
+          start (CountDownLatch. 1)
+          done (CountDownLatch. n)
+          durable (default-durable tmp-file {} {:write slow-write})
+          expected (reduce #(assoc %1 %2 true) {} (range n))]
+      (doseq [x (range n)]
+        (future-swap start done durable assoc x true))
+      (.countDown start)
+      (.await done)
+      (is (= expected @durable))
+      (is (= expected (slurp-data tmp-file))))))
